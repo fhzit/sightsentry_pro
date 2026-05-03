64535fbca53a0b21d611ff12026b08d7f9158f74
HelloFHZ <152045732+fhzit@users.noreply.github.com>
2026-05-03 14:19:06 +0800

    Update build.gradle.kts

--- src-tauri/build.gradle.kts
+++ src-tauri/build.gradle.kts
@@ -1,42 +1,17 @@
-android {
-    compileSdkVersion 34
-    targetSdkVersion 34
-    minSdkVersion 28
-    
-    ndkVersion = "25.2.9519663"
-    
-    buildToolsVersion = "34.0.0"
-    
-    defaultConfig {
-        applicationId = "com.sightsentry.pro"
-        manifestPlaceholders += [
-            bluetoothScanPermission: "android.permission.BLUETOOTH_SCAN",
-            bluetoothConnectPermission: "android.permission.BLUETOOTH_CONNECT",
-            bluetoothPermission: "android.permission.BLUETOOTH",
-            accessFineLocationPermission: "android.permission.ACCESS_FINE_LOCATION",
-            accessCoarseLocationPermission: "android.permission.ACCESS_COARSE_LOCATION",
-            nearbyWifiDevicesPermission: "android.permission.NEARBY_WIFI_DEVICES"
-        ]
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
     }
-    
-    compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
-    }
-    
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    
-    packagingOptions {
-        jniLibs {
-            useLegacyPackaging = true
-        }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
     }
 }
 
-if (project.hasProperty("ANDROID_HOME")) {
-    Properties properties = new Properties()
-    properties.load(file("local.properties").newDataInputStream())
-    android.sdkDirectory = properties.getProperty("sdk.dir")
+allprojects {
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
