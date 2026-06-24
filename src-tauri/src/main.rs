535f451ad3b3b6f3ed1ad46cc2791fb8126f4553
fhzit <fhzit@users.noreply.github.com>
2026-06-24 12:30:43 +0000

    chore: 删除旧版构建文件

--- src-tauri/src/main.rs
+++
@@ -1,9 +0,0 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-#[cfg(not(target_os = "android"))]
-fn main() {
-    sightsentry_pro_lib::run()
-}
-
-#[cfg(target_os = "android")]
-fn main() {}
